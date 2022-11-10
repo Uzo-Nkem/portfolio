8e9ad8f09e8bd4b551b94e6e0c12b04e81cc7b94
Uzo-Nkem <110984273+Uzo-Nkem@users.noreply.github.com>
2022-11-10 11:01:30 +0000

    Uploaded portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const toggleButton = document.getElementsByClassName('toggle_button')[0]
-const navbar = document.getElementsByTagName('ul')[0]
+const navbar = document.getElementsByClassName('nav-links')[0]
 
 toggleButton.addEventListener('click', () => {
 navbar.classList.toggle('active')
 })
+
+window.addEventListener("scroll", function(){
+    const navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("sticky", window.scrollY > 0);
+}) 
